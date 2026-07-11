c029e3865f76504674d0d650353ddc158b5b47f3
Mark Koszykowski <31666939+markkoszykowski@users.noreply.github.com>
2026-07-11 16:46:34 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,34 @@
+class MinStack {
+    private final Deque<Integer> descending = new ArrayDeque<>();
+    private final Deque<Integer> ascending = new ArrayDeque<>();
+    private final Deque<Integer> stack = new ArrayDeque<>();
+    
+    public MinStack() {}
+    
+    public void push(final int val) {
+        this.stack.addLast(val);
+        if (this.descending.isEmpty() || val < this.descending.getLast()) {
+            this.descending.addLast(val);
+        } else {
+            this.ascending.addLast(val);
+        }
+    }
+    
+    public void pop() {
+        final int last = this.stack.getLast();
+        this.stack.removeLast();
+        if (!this.ascending.isEmpty() && this.ascending.getLast() == last) {
+            this.ascending.removeLast();
+        } else {
+            this.descending.removeLast();
+        }
+    }
+    
+    public int top() {
+        return this.stack.getLast();
+    }
+    
+    public int getMin() {
+        return this.descending.getLast();
+    }
+}
