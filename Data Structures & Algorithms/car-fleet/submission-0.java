344197f16eb3ba772649e9480d7098331cb61792
Mark Koszykowski <31666939+markkoszykowski@users.noreply.github.com>
2026-07-11 15:10:39 -0400

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    private static boolean catchesUp(
+        final int target, 
+        final int ahead, final int behind,
+        final int[] position, final int[] speed
+    ) {
+        final double aheadTime = ((double)(target - position[ahead])) / speed[ahead];
+        final double behindTime = ((double)(target - position[behind])) / speed[behind];
+        return behindTime <= aheadTime;
+    }
+
+    public int carFleet(final int target, final int[] position, final int[] speed) {
+        final Integer[] cars = new Integer[position.length];
+        for (int i = 0; i != cars.length; ++i) {
+            cars[i] = i;
+        }
+        Arrays.sort(cars, (left, right) -> { return position[right] - position[left]; });
+        
+        int fleets = 1;
+        int previous = 0;
+        for (int i = 1; i != cars.length; ++i) {
+            if (!catchesUp(target, cars[previous], cars[i], position, speed)) {
+                fleets += 1;
+                previous = i;
+            }
+        }
+        
+        return fleets;
+    }
+}
